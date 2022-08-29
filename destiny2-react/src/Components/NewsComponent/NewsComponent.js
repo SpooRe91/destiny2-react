15ab395f4172bb8a2a5699758dfe685c41c118d8
Martin B <m.bogdanov9110@gmail.com>
2022-08-29 17:15:36 +0300

    created News Component for each news article

---
+++ destiny2-react/src/Components/NewsComponent/NewsComponent.js
@@ -0,0 +1,12 @@
+export const NewsComponent = ({ data }) => {
+    return (
+        <div className="news-element">
+            <article key={data.identifier}>
+                <a href={`https://www.bungie.net${data.link}`} target="_blank" rel="noreferrer"><h3>{data.displayName}</h3>
+                    <img src={`https://www.bungie.net${data.image}`} alt="#" />
+                </a>
+            </article>
+        </div>
+    )
+
+}
