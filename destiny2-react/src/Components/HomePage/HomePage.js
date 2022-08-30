8965436edfa037d8651847ef3240cd04efb02c25
Martin B <m.bogdanov9110@gmail.com>
2022-08-30 14:31:18 +0300

    updates on HomePage component

--- destiny2-react/src/Components/HomePage/HomePage.js
+++ destiny2-react/src/Components/HomePage/HomePage.js
@@ -1,37 +1,18 @@
 import { useEffect, useState } from "react";
 import { ClockLoader } from "react-spinners";
 
-import { NewsComponent } from "../NewsComponent/NewsComponent";
-
+import { ClanMemberComponent } from "./ClanMemberComponent";
+import styles from "./HomePage.module.css";
 export const HomePage = () => {
 
-    const [news, setNews] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [clanData, setClanData] = useState(null);
-    const [toShowNews, setToShowNews] = useState(false);
+    const [toShowMembers, setToShowMembers] = useState(false);
+    const [clanMembers, setClanMembers] = useState(null);
 
     const API_KEY = process.env.REACT_APP_BUNGIE_API_KEY;
 
     useEffect(() => {
-        fetch('https://www.bungie.net/Platform/Trending/Categories/', {
-            headers: {
-                'content-type': 'application/json',
-                "X-API-Key": API_KEY
-            }
-        })
-            .then(res => res.json())
-            .then(data => {
-                if (data.Response?.categories[1].entries.results.length > 0) {
-                    setNews(data.Response?.categories[1].entries.results);
-                    setIsLoading(state => false);
-                } else {
-                    setIsLoading(state => true);
-                    throw new Error('Unable to fetch news, please try again later!')
-                }
-            }).catch(error => {
-                alert(error);
-            })
-
         fetch('https://www.bungie.net/Platform/GroupV2/4131725', {
             headers: {
                 'content-type': 'application/json',
@@ -42,20 +23,33 @@ export const HomePage = () => {
             .then(data => {
                 if (data?.ErrorCode !== 2101 && data?.Response?.detail !== null && data.Response?.detail !== undefined) {
                     setClanData(data.Response?.detail);
+                    setIsLoading(state => false)
                 } else {
                     throw new Error("Unable to fetch clan info, please try again later!");
                 }
             })
             .catch(error => {
                 alert(error);
-            })
+            });
+
+        fetch('https://www.bungie.net/Platform/GroupV2/4131725/Members/', {
+            headers: {
+                'content-type': 'application/json',
+                "X-API-Key": API_KEY
+            }
+        })
+            .then(res => res.json())
+            .then(data => setClanMembers(data.Response.results))
+            .catch(error => {
+                alert(error);
+            });
     }, [API_KEY]);
 
     const creation = new Date(clanData?.creationDate);
-
-    console.log(clanData);
+    console.log(clanMembers)
     return (
         <>
+        <title>Home</title>
             <h1 style={{ 'fontSize': '25px', 'color': 'white', 'margin': 'auto', 'background': '#000000ce', "padding": '10px', 'width': 'fit-content' }}>
                 Welcome to the BGs Destiny 2 clan, website!
                 <p style={{ 'fontSize': '14px' }}>
@@ -70,54 +64,36 @@ export const HomePage = () => {
                         :
                         clanData
                         &&
-                        <div>
-                            <h1 style={{ 'fontSize': '25px', 'color': 'white', 'margin': 'auto 15px', "padding": '10px', "textDecoration": "underline" }}>CLAN INFO</h1>
-                            <h3>{clanData?.name}</h3>
-                            <p>Since: {`${creation?.getDate()}/${creation?.getMonth() + 1}/${creation?.getFullYear()}`}</p>
-                            <p>Our moto: </p>
-                            <h4>{clanData?.motto}</h4>
-                            <h4>Callsign: {clanData?.clanInfo.clanCallsign}</h4>
+                        <>
                             <div>
-                                <article>
-                                    {clanData?.about}
-                                </article>
-                            </div>
-                            <p>Visit our clan in Bungie.net - <a href={'https://www.bungie.net/en/ClanV2?groupid=4131725'} style={{ 'color': 'coral', 'fontWeight': 'bold' }} target="_blank" rel="noreferrer">
-                                HERE!
-                            </a></p>
+                                <h1 style={{ 'fontSize': '25px', 'color': 'white', 'margin': 'auto 15px', "padding": '10px', "textDecoration": "underline" }}>CLAN INFO</h1>
+                                <h3>{clanData?.name}</h3>
+                                <p>Since: {`${creation?.getDate()}/${creation?.getMonth() + 1}/${creation?.getFullYear()}`}</p>
+                                <p>Our moto: </p>
+                                <h4>{clanData?.motto}</h4>
+                                <h4>Callsign: {clanData?.clanInfo.clanCallsign}</h4>
+                                <div>
+                                    <article>
+                                        {clanData?.about}
+                                    </article>
+                                </div>
+                                <button className={styles["showMembers"]} onClick={() => setToShowMembers(state => !state)} style={toShowMembers ? { 'color': 'coral' } : { 'color': 'lightblue' }}>
+                                    {!toShowMembers ? "Show clan members" : "Hide clan members"}
 
-                        </div>
+                                </button>
+                            </div>
+                            <div className={styles["clan-member-container"]}>
+                                {toShowMembers && clanMembers?.sort((a, b) => b.memberType - a.memberType)
+                                    .map(el =>
+                                        <h1 key={el.destinyUserInfo.membershipId}>
+                                            {el.memberType === 5 ? 'Clan Owner' : ""} <ClanMemberComponent key={el.destinyUserInfo.membershipId} data={el} />
+                                        </h1>
+                                    )}
+                            </div>
+                        </>
                     }
                 </div>
-            }
-            {
-                <>
-                    <h1 style={{ 'fontSize': '25px', 'color': 'white', 'margin': 'auto 15px', 'background': '#000000ce', "padding": '10px' }}>NEWS</h1>
-                    <div className="newsContainer">
-                        {
-                            <>
-                                {isLoading
-                                    ?
-                                    <ClockLoader color="lightblue" size="50px" />
-                                    :
-                                    <>
-                                        <button onClick={() => setToShowNews(state => !state)} className="newsButton"
-                                            style={toShowNews ? { 'color': 'coral' } : { 'color': 'lightblue' }}>
-                                            {toShowNews ? 'Show latest news' : 'Show ALL Bungie.net news'}
-                                        </button>
 
-                                        {!toShowNews
-                                            ?
-                                            news?.slice(0, 4).map(el => <NewsComponent key={el.identifier} data={el} />)
-                                            :
-                                            news?.map(el => <NewsComponent key={el.identifier} data={el} />)}
-                                    </>
-                                }
-                            </>
-
-                        }
-                    </div>
-                </>
             }
         </>
     )
