db7a7b140538460a57ca90d73a26a8c01f09f8b1
Martin B <m.bogdanov9110@gmail.com>
2022-08-30 14:30:00 +0300

    added routes to App.js

--- destiny2-react/src/App.js
+++ destiny2-react/src/App.js
@@ -1,11 +1,21 @@
 import '../src/css/styles.css';
-import { HomePage } from './Components/HomePage/HomePage';
+import { Route, Routes } from "react-router-dom"
 
+import { HomePage } from './Components/HomePage/HomePage';
+import { NavBar } from './Components/NavBar/NavBar';
+import { NewsPage } from './Components/NewsComponent/NewsPage';
+import { MusicComponent } from './Components/MusicComponent';
 function App() {
 
   return (
     <div className="App">
-      <HomePage />
+      <NavBar />
+      <MusicComponent />
+      <Routes>
+
+        <Route path='/' element={<HomePage />} />
+        <Route path='/bungie/news' element={<NewsPage />} />
+      </Routes>
     </div>
   );
 }
