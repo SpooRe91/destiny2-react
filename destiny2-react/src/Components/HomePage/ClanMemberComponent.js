00d3dd015146c48015e121aea0c17d3fe306b957
Martin B <m.bogdanov9110@gmail.com>
2022-08-30 14:30:27 +0300

    createdClanMemberComponent

---
+++ destiny2-react/src/Components/HomePage/ClanMemberComponent.js
@@ -0,0 +1,26 @@
+import styles from "./HomePage.module.css";
+
+export const ClanMemberComponent = ({ data }) => {
+    const dateJoined = new Date(data?.joinDate);
+
+    return (
+
+        <div>
+            <p className={styles["player-name"]}>
+                Name: <a href={`https://www.bungie.net/7/en/User/Profile/${data.bungieNetUserInfo.membershipType}/${data.bungieNetUserInfo.membershipId}`}
+                    target="_blank" rel="noreferrer">
+                    <strong>
+                        {data?.bungieNetUserInfo.bungieGlobalDisplayName}
+                    </strong>
+                </a>
+            </p>
+            <p className={styles["player-joined"]}>
+                Joined: <strong>{`${dateJoined.getDate()}/${dateJoined.getMonth() + 1}/${dateJoined.getFullYear()}`}</strong>
+            </p>
+            <a href={`https://www.bungie.net/7/en/User/Profile/${data.bungieNetUserInfo.membershipType}/${data.bungieNetUserInfo.membershipId}`}
+                target="_blank" rel="noreferrer">
+                <img src={`https://www.bungie.net/${data?.bungieNetUserInfo.iconPath}`} alt="#" className={styles["player-avatar"]} />
+            </a>
+        </div >
+    )
+}
