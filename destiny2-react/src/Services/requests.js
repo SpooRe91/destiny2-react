0845c32dcca442556d8176d06d32688091f92f94
Martin B <m.bogdanov9110@gmail.com>
2023-07-26 18:41:46 +0300

    Updated reqests to be sent to personal API

--- destiny2-react/src/Services/requests.js
+++ destiny2-react/src/Services/requests.js
@@ -1,67 +1,82 @@
 const API_KEY = process.env.REACT_APP_BUNGIE_API_KEY;
+const URL = "https://mb-cook-server.vercel.app/d2";
 
 export const getClanInfo = (signal, controller) => {
-
-    return fetch('https://www.bungie.net/Platform/GroupV2/4131725', {
-        signal: signal,
-        headers: {
-            'content-type': 'application/json',
-            "X-API-Key": API_KEY
-        }
-    })
-        .then(res => res.json())
-        .then(data => data)
-        .catch(error => {
-            if (controller.signal.aborted) { return }
-            console.log(error);
-        });
+  return fetch(`${URL}/clanInfo`, {
+    signal: signal,
+    headers: {
+      "content-type": "application/json",
+      "X-API-Key": API_KEY,
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => data)
+    .catch((error) => {
+      if (controller.signal.aborted) {
+        return;
+      }
+      console.log(error);
+    });
 };
 
 export const getClanMembers = (signal, controller) => {
-
-    return fetch('https://www.bungie.net/Platform/GroupV2/4131725/Members/', {
-        signal: signal,
-        headers: {
-            'content-type': 'application/json',
-            "X-API-Key": API_KEY
-        }
-    })
-        .then(res => res.json())
-        .then(data => data)
-        .catch(error => {
-            if (controller.signal.aborted) { return }
-            console.log(error);
-        });
+  return fetch(`${URL}/getClanMembers`, {
+    signal: signal,
+    headers: {
+      "content-type": "application/json",
+      "X-API-Key": API_KEY,
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => data)
+    .catch((error) => {
+      if (controller.signal.aborted) {
+        return;
+      }
+      console.log(error);
+    });
 };
 
 export const getUser = (id, signal, controller) => {
+  return fetch(`${URL}/getUser/${id}`, {
+    signal: signal,
+    headers: {
+      "content-type": "application/json",
+      "X-API-Key": API_KEY,
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => data)
+    .catch((error) => {
+      if (controller.signal.aborted) {
+        return;
+      }
+      console.log(error);
+    });
+};
 
-    return fetch(`https://www.bungie.net/Platform/User/GetBungieNetUserById/${id}/`, {
-        signal: signal,
-        headers: {
-            'content-type': 'application/json',
-            "X-API-Key": API_KEY
-        }
-    })
-        .then(res => res.json())
-        .then(data => data)
-        .catch(error => {
-            if (controller.signal.aborted) { return }
-            console.log(error);
-        })
-}
-
-export const getCharacter = (membershipType, destinyMembershipId, signal, controller) => {
-    return fetch(`https://www.bungie.net/Platform/Destiny2/${membershipType}/Account/${destinyMembershipId}/Character/0/Stats`, {
-        signal: signal,
-        headers: {
-            'content-type': 'application/json',
-            "X-API-Key": API_KEY
-        }
-    })
-        .then(res => res.json())
-        .catch(error => {
-            if (controller.signal.aborted) { return }
-            console.log(error);
-        });
-}
+export const getCharacter = (
+  membershipType,
+  destinyMembershipId,
+  signal,
+  controller
+) => {
+  return fetch(`${URL}/getCharacter/${membershipType}/${destinyMembershipId}`, {
+    signal: signal,
+    headers: {
+      "content-type": "application/json",
+      "X-API-Key": API_KEY,
+    },
+    body: {
+      membershipType,
+      destinyMembershipId,
+    },
+  })
+    .then((res) => res.json())
+    .catch((error) => {
+      if (controller.signal.aborted) {
+        return;
+      }
+      console.log(error);
+    });
+};
