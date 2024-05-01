49bc79768e92fbda28e9d0e9c9a4cafe346bd387
Martin B <m.bogdanov9110@gmail.com>
2024-05-01 13:11:39 +0300

    fix: fixed iFrames responsiveness and additional stylings

--- destiny2-react/src/Components/VideosComponent/VideoComponent.js
+++ destiny2-react/src/Components/VideosComponent/VideoComponent.js
@@ -1,25 +1,29 @@
-import { useNavigate, useParams } from "react-router"
-
-import styles from "./VideoComponent.module.css"
+import { useParams } from "react-router";
+import styles from "./VideoComponent.module.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
-export const VideoComponent = ({ videoName }) => {
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { NavLink } from "react-router-dom";
 
+export const VideoComponent = ({ videoName }) => {
     const { id } = useParams();
-    const navigate = useNavigate();
 
     return (
         <div className={styles["all-videos-container"]}>
             <title>{videoName}</title>
             <h1 className={styles["video-header"]}>{videoName ? videoName : "Current video"}</h1>
             <div className={styles["button-holder"]}>
-                <button onClick={() => navigate('/bgs/videos')} className={styles["back-button"]}><FontAwesomeIcon icon={faArrowLeft} /> Back</button>
+                <NavLink to={'/bgs/videos'} className={styles["back-button"]}><FontAwesomeIcon icon={faArrowLeft} /> Back</NavLink>
             </div>
             <div className={styles["single-video-container"]}>
-                <iframe
-                    src={`https://www.youtube.com/embed/watch?${id}?autoplay=0`} title="#" loading="lazy">
-                </iframe>
+                <div className={styles["video-wrapper"]}>
+                    <iframe
+                        src={`https://www.youtube.com/embed/watch?${id}?autoplay=0`}
+                        title="#"
+                        loading="lazy"
+                        className={styles["video-iframe"]}
+                    />
+                </div>
             </div>
         </div>
-    )
-}
+    );
+};
